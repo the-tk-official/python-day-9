2121665534f0864bd890f8aa842d4ba7a70e505e
the_tk_official <the.tk.official@gmail.com>
2021-10-08 11:18:08 +0300

    Day 9

---
+++ Blind Auction.py
@@ -0,0 +1,31 @@
+from art import logo
+from os import system
+
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+while not bidding_finished:
+    name = str(input('What is your name? '))
+    price = int(input('What is your bid? $'))
+
+    bids[name] = price
+    should_continue = input('Are there any other bidders? Type "yes" or "no".\n')
+
+    if should_continue == 'no':
+        bidding_finished = True
+    elif should_continue == 'yes':
+        system('cls')
+
+find_highest_bidder(bidding_record=bids)
